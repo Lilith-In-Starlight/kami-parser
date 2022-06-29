6f3e2e2da93388b62471af5a60c97e5b80217c9c
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-06-29 12:21:35 -0500

    Turned into a command line tool

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod lexer;
-mod syntax;
+
 use std::fs;
+use std::env;
 
 fn main() {
-	let input = fs::read_to_string("example.kami").unwrap();
-	fs::write("example.html", lexer::parse_multiline(&input)).unwrap();
+	let args: Vec<String> = env::args().collect();
+	if args.len() == 2 {
+		let input = fs::read_to_string(&args[1]).unwrap();
+		fs::write(&args[1], lexer::parse_multiline(&input)).unwrap();
+	}
 }
