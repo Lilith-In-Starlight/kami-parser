456464cf48da8cee8defff84513bd02f1e7c4266
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-06-29 12:28:33 -0500

    Command line fixes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() == 2 {
 		let input = fs::read_to_string(&args[1]).unwrap();
-		fs::write(&args[1], lexer::parse_multiline(&input)).unwrap();
+		fs::write(&args[1].replace(".kami", ".html"), lexer::parse_multiline(&input)).unwrap();
 	}
 }
