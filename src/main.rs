2201e2707bc8f22f7b9c5420e8e6b173a7e1bd8b
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-06-30 09:07:55 -0500

    Separated the parser from lexer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-mod lexer;
+mod syntax;
 
 use std::fs;
 use std::env;
@@ -7,6 +7,6 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() == 2 {
 		let input = fs::read_to_string(&args[1]).unwrap();
-		fs::write(&args[1].replace(".kami", ".html"), lexer::parse_multiline(&input)).unwrap();
+		fs::write(&args[1].replace(".kami", ".html"), syntax::parse_multiline(&input)).unwrap();
 	}
 }
