5b9d6e5f9adda6ec6ac9ed975322decf1d2ca35f
Katie And <56773311+Lilith-In-Starlight@users.noreply.github.com>
2022-06-30 10:22:58 -0500

    Stopped program from overriding original file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,11 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() == 2 {
 		let input = fs::read_to_string(&args[1]).unwrap();
-		fs::write(&args[1].replace(".kami", ".html"), syntax::parse(&input)).unwrap();
+		let outpath = &args[1].replace(".km", ".html");
+		if outpath != &args[1] {
+			fs::write(&args[1].replace(".kami", ".html"), syntax::parse(&input)).unwrap();
+		} else {
+			panic!("Output path was the same as input path, are you sure the extension is .km?");
+		}
 	}
 }
